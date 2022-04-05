86e5c2ec94192e282e8e34490d573a9395f327c4
Steven Pjontek <77060535+spjontek04@users.noreply.github.com>
2022-04-05 09:40:26 -0500

    yws

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
- 
 <!doctype html>
 <html lang="en">
   <head>
@@ -18,40 +17,11 @@
     <link rel="icon" type="image/png" sizes="16x16" href="./Images/favicon-16x16.png">
     <link rel="manifest" href="./Images/site.webmanifest">
     <!--End Of Favicon-->
-    <title>Home</title>
+    <title>Header</title>
   </head>
   <body>
-
-  <h1>Welcome Coach B</h1>
-  <br>
-  <h2>Come check out these dope pages:</h2>
-  <?php
-    include "header.php";
-
-    
-  ?>
-
-  <?php
-    echo $_POST['name'];
-  ?>
-  
-    
- 
-
-  <form method="POST">
-    <input type="hidden" name="name" value="Steven">
-    <button type="submit">Press Me for Name</button>
-  </form>
-    
-    
-
-  <?php
-    setcookie("name", "Steven", time() - 48);
-  ?>
-
-  <?php 
-    include_once 'footer.php';
-  ?>
+      
+      
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -59,11 +29,4 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
-
-
-
-
-
-
-
 
